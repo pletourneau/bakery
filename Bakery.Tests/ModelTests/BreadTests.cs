12445e2f887c8212c8ad9b2ea59acd7d7657822f
Paul LeTourneau <pletourneau62@gmail.com>
2023-09-22 08:55:34 -0700

    good fail test one

---
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class BreadTests
+  {
+    [TestMethod]
+    public void BreadConstructor_CreatesInstanceOfBread_Bread()
+    {
+      Bread newBread = new Bread(2);
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+  }
+}
+// NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedReturnValue()
