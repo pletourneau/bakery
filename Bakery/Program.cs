87389bb5753971d2a21f4a4bfddc0cc07cd0569f
Paul LeTourneau <pletourneau62@gmail.com>
2023-09-22 16:42:08 -0700

    finish styling

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -8,13 +8,16 @@ namespace Bakery
   {
     static void Main()
     {
-      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.Clear();
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*");
       Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("---------------------------");
       Console.WriteLine("Each bread loaf is $5"); 
-      Console.WriteLine("__buy two, get one free!");
+      Console.WriteLine("--buy two, get one free!");
       Console.WriteLine("Each pastry is $2");
-      Console.WriteLine("__buy four, get one free!");
-      Console.WriteLine("_________________________________");
+      Console.WriteLine("--buy four, get one free!");
+      Console.WriteLine("-------------------------");
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*");
       Console.WriteLine("How many pastries for you?");
       string pastryString = Console.ReadLine();
       Console.WriteLine("How many loaves of bread for you?");
@@ -26,11 +29,13 @@ namespace Bakery
         int userInputPastry = int.Parse(pastryString);
         CalcPrice(userInputBread, userInputPastry);
         Console.WriteLine("Would you like to price out another order? Type yes to start another order");
+        
 
         string answer = Console.ReadLine();
 
         if (answer.ToLower() == "yes" || answer.ToLower() == "y") 
         {
+          Console.Clear();
           Main();
         }
         else
@@ -52,9 +57,11 @@ namespace Bakery
       int brTot = breadNum.PriceTotal();
       int paTot = pastryNum.PriceTotalP();
       int total =  brTot + paTot;
-
-      Console.WriteLine($"Let's see, for {userInputBread} loaves of bread it will cost {brTot} and for {userInputPastry} pastries it will cost {paTot} so....");
+      Console.Clear();
+      Console.WriteLine("---------------------------------------------");
+      Console.WriteLine($"Let's see, {userInputBread} loaves of bread will cost {brTot} and {userInputPastry} pastries will cost {paTot} so....");
       Console.WriteLine($"That will be {total} dollars s'il vous plaît");
+      Console.WriteLine("---------------------------------------------");
     }
   }
 }
