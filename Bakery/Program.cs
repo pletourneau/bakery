b7130c95f43801fc9e7984ea89611231a93846cd
Paul LeTourneau <pletourneau62@gmail.com>
2023-09-22 11:18:37 -0700

    WIP UI

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -11,8 +11,15 @@ namespace Bakery
       Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
       Console.WriteLine("Welcome to your Bakery!");
       Console.WriteLine("Each bread loaf is $5, and every third loaf is free");
-      Console.WriteLine("Each pastry is $5, and every fourth loaf is free");
-      Console.WriteLine("How many pastries and loaves of bread would you like?");
+      Console.WriteLine("Each pastry is $2, and every fourth loaf is free");
+      Console.WriteLine("How many pastries would you like?");
+      string pastryString = Console.ReadLine();
+      Console.WriteLine("How many loaves of bread would you like?");
+      string breadString = Console.ReadLine();
+
+      int userInputBread = int.Parse(breadString);
+      int userInputPastry = int.Parse(pastryString);
+      Console.WriteLine($"did you want {userInputBread} loaf/loaves?");
     }
   }
 }
